b747634d42d3101b5c3e3c57fd8d61e1b4e66cde
ahssa96 <ahssa96@naver.com>
2024-07-12 16:58:09 +0900

    modified:   react-project/src/components/06_Child.jsx 	modified:   react-project/src/pages/06_PropsPage.jsx

--- react-project/src/components/06_Child.jsx
+++ react-project/src/components/06_Child.jsx
@@ -1,18 +1,31 @@
-import CatImage from "../assets/cat.jpg"
+// import CatImage from "../assets/cat.jpg"
 import PropTypes from "prop-types"
-export default function Child({ size }) {
+
+export default function Child({ size, cat }) {
     // const catImage = "../assets/cat.jpg";
+    console.log(cat);
     const width = 1.33 * size;
 
+    // return (
+    //     <img src={CatImage}
+    //         alt="고양이"
+    //         width={width}
+    //         height={size}
+    //     ></img >
+    // );
+
     return (
-        <img src={CatImage}
-            alt="고양이"
-            width={width}
-            height={size}
-        ></img >
-    );
+        <img src={cat.imageURL} alt={cat.name} width={width} height={size}></img>
+    )
 }
 
 Child.propTypes = {
+    //size를 number타입, 필수로 입력을 받는다
     size: PropTypes.number.isRequired,
+
+    //shape : 자바스크립트 객체 타입 검사를 할때
+    cat: PropTypes.shape({
+        imageURL: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+    })
 };
--- react-project/src/pages/06_PropsPage.jsx
+++ react-project/src/pages/06_PropsPage.jsx
@@ -1,7 +1,11 @@
 import Child from "../components/06_Child";
+import CatImage from "../assets/cat.jpg"
 
 export default function PropsPage() {
+    // return (
+    //     <Child size={500} />
+    // );
     return (
-        <Child size={500} />
+        <Child size={500} cat={{ name: "고양이", imageURL: CatImage }} />
     );
 }
